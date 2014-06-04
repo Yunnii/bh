81ccb5c7c47cd2bd865e3c3abe3997fcd5f13732
Mikhail Troshev <mishanga@yandex-team.ru>
2014-06-04 16:21:16 +0400

    coverage increase

--- test/test.match.js
+++ test/test.match.js
@@ -7,8 +7,25 @@ describe('bh.match()', function() {
         bh = new BH();
     });
 
+    it('should not create invalid matcher', function() {
+        bh.match(false, function() {});
+        bh.apply('').should.equal('');
+    });
+
     it('should allow to use chaining', function() {
         bh.match('button', function() {}).should.eql(bh);
+    });
+
+    it('should match on elem of block with modifier', function() {
+        bh.match('button_disabled__control', function(ctx) {
+            ctx.tag('input');
+        });
+
+        bh.apply({
+            block: 'button',
+            mods: { disabled: true },
+            content: { elem: 'control' }
+        }).should.equal('<div class="button button_disabled"><input class="button__control"/></div>');
     });
 
     it('should allow to use a few matchers in one call', function() {
